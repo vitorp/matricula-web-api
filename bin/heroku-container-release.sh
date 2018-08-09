adb458c0c58e3f412c1bc22c340159fadd330036
vitor pontes <vitormax2005@hotmail.com>
2018-08-08 22:02:45 -0300

    Add more docker configuration

--- bin/heroku-container-release.sh
+++ bin/heroku-container-release.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-docker tag vitorp/rails registry.heroku.com/grade-web-api/web
+docker tag vitorp/matricula-web-api registry.heroku.com/matricula-web-api/web
 docker login --username=_ --password=$HEROKU_API_KEY registry.heroku.com
-docker push registry.heroku.com/grade-web-api/web
+docker push registry.heroku.com/matricula-web-api/web
 
-imageId=$(docker inspect registry.heroku.com/grade-web-api/web --format={{.Id}})
+imageId=$(docker inspect registry.heroku.com/matricula-web-api/web --format={{.Id}})
 payload='{"updates":[{"type":"web","docker_image":"'"$imageId"'"}]}'
-curl -n -X PATCH https://api.heroku.com/apps/grade-web-api/formation \
+curl -n -X PATCH https://api.heroku.com/apps/matricula-web-api/formation \
 -d "$payload" \
 -H "Content-Type: application/json" \
 -H "Accept: application/vnd.heroku+json; version=3.docker-releases" \
