4823bb9d69bbe5d126ef9560922d515d9ed0e6e8
vitor pontes <vitormax2005@hotmail.com>
2018-09-07 10:10:26 -0300

    Add flow subjects scope by_period

--- spec/models/flow_spec.rb
+++ spec/models/flow_spec.rb
@@ -3,4 +3,27 @@ require "rails_helper"
 RSpec.describe Flow, type: :model do
   it { is_expected.to belong_to :curriculum }
   it { is_expected.to have_many :flow_subjects }
+  it { is_expected.to have_many :subjects }
+
+  describe ".subjects_by_period" do
+    subject(:subjects_by_period) { flow.subjects.by_period(selected_period) }
+
+    let(:flow) { create(:flow) }
+    let(:subjects_order1) { create_list(:flow_subject, 1, flow: flow, order: 1).map(&:subject) }
+    let(:subjects_order2) { create_list(:flow_subject, 2, flow: flow, order: 2).map(&:subject) }
+
+    context "when the period is 1" do
+      let(:selected_period) { 1 }
+
+      it { is_expected.to include(*subjects_order1) }
+      it { is_expected.not_to include(*subjects_order2) }
+    end
+
+    context "when the period is 2" do
+      let(:selected_period) { 2 }
+
+      it { is_expected.to include(*subjects_order2) }
+      it { is_expected.not_to include(*subjects_order1) }
+    end
+  end
 end
