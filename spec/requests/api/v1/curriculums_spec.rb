aaa18275305d01c8baf77c483d5abaf4d78e6174
vitor pontes <vitormax2005@hotmail.com>
2018-09-09 15:47:26 -0300

    Add ActiveModelSerializer and curriculum serializer

--- spec/requests/api/v1/curriculums_spec.rb
+++ spec/requests/api/v1/curriculums_spec.rb
@@ -10,16 +10,33 @@ RSpec.describe "Curriculums", type: :request do
 
     before do
       create(:curriculum) # curriculum from other courses
+      curriculums_request
     end
 
-    it "has http status ok" do
+    it { expect(response).to have_http_status(:ok) }
+    it { expect(json_response.size).to eq 1 }
+
+    it "returns curriculum course" do
+      expect(json_response["data"][0]["relationships"]["course"]["data"]["id"]).to eq course.id
+    end
+  end
+
+  describe "GET /curriculums/:id" do
+    subject(:curriculums_request) { get api_v1_curriculum_path(curriculum) }
+
+    let(:curriculum) { create(:curriculum) }
+    let(:course) { curriculum.course }
+    let(:json_response) { JSON.parse(response.body) }
+
+    before do
       curriculums_request
-      expect(response).to have_http_status(:ok)
     end
 
-    it "returns only course curriculum" do
-      curriculums_request
-      expect(json_response.size).to eq 1
+    it { expect(response).to have_http_status(:ok) }
+    it { expect(json_response["data"]["id"]).to eq curriculum.id.to_s }
+
+    it "returns curriculum course" do
+      expect(json_response["data"]["relationships"]["course"]["data"]["id"]).to eq course.id
     end
   end
 end
