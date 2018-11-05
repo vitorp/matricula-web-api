396be457603396bb28c9d65db16a62ae51a0221a
vitor pontes <vitormax2005@hotmail.com>
2018-11-05 21:35:40 -0200

    Fix offer serializer credits names

--- app/serializers/offer_serializer.rb
+++ app/serializers/offer_serializer.rb
@@ -1,6 +1,6 @@
 class OfferSerializer < ActiveModel::Serializer
-  attributes :id, :class_letter, :shift, :enrolled_count, :max_enrolled, :credits_theory, :credits_extension,
-             :credits_pratical, :credits_study, :obs1, :obs2, :obs3
+  attributes :id, :class_letter, :shift, :enrolled_count, :max_enrolled, :credits_of_theory, :credits_of_extension,
+             :credits_of_practice, :credits_of_study, :obs1, :obs2, :obs3
   belongs_to :semester
   belongs_to :subject
 end
