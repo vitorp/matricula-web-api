d9c423fa57ab8af52cee372f99c46dae0062bc01
alexnsouza6 <alexnsouza6@hotmail.com>
2018-11-09 14:41:53 -0200

    feat: adding validations test for course attrs

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,6 +1,13 @@
 require "rails_helper"
 
 RSpec.describe Course, type: :model do
-  it { is_expected.to have_many :curriculums }
-  it { is_expected.to belong_to :department }
+  context "when testing associations..." do
+    it { is_expected.to have_many :curriculums }
+    it { is_expected.to belong_to :department }
+  end
+
+  context "when testing validations..." do
+    it { is_expected.to define_enum_for(:shift).with(%i[day night]) }
+    it { is_expected.to define_enum_for(:modality).with(%i[presential remote]) }
+  end
 end
