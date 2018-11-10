ad9626104ece6be35f7bc450bccbd270fa33ce11
alexnsouza6 <alexnsouza6@hotmail.com>
2018-11-10 12:36:19 -0200

    feat: enforcement validation and its test

--- app/models/curriculums_subject.rb
+++ app/models/curriculums_subject.rb
@@ -1,9 +1,12 @@
 class CurriculumsSubject < ApplicationRecord
+  # Associations
   belongs_to :curriculum
   belongs_to :subject
 
+  # Validations
   enum enforcement: {
     required: 0,
     optional: 1
   }
+  validates :enforcement, presence: true
 end
--- spec/models/curriculums_subject_spec.rb
+++ spec/models/curriculums_subject_spec.rb
@@ -1,6 +1,13 @@
 require "rails_helper"
 
 RSpec.describe CurriculumsSubject, type: :model do
-  it { is_expected.to belong_to :curriculum }
-  it { is_expected.to belong_to :subject }
+  context "when testing associations..." do
+    it { is_expected.to belong_to(:curriculum) }
+    it { is_expected.to belong_to(:subject) }
+  end
+
+  context "when testing validations..." do
+    it { is_expected.to validate_presence_of(:enforcement) }
+    it { is_expected.to define_enum_for(:enforcement).with(%i[required optional]) }
+  end
 end
