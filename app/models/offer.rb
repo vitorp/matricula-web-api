071f6aef631f1db1c97257cbcf7808711ef9d452
alexnsouza6 <alexnsouza6@hotmail.com>
2018-11-10 12:41:36 -0200

    feat: validations w/ tests and refactoring associations tests

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,11 +1,27 @@
 class Offer < ApplicationRecord
+  # Associations
   belongs_to :subject
   belongs_to :semester
   has_many :offer_professors, dependent: :destroy
   has_many :professors, through: :offer_professors
 
+  # Validations
   enum shift: {
     day:   0,
     night: 1
   }
+
+  with_options presence: true do
+    validates :class_letter
+    validates :shift
+    validates :enrolled_count
+    validates :max_enrolled
+    validates :credits_of_theory
+    validates :credits_of_extension
+    validates :credits_of_practice
+    validates :credits_of_study
+    validates :obs1
+    validates :obs2
+    validates :obs3
+  end
 end
--- spec/models/offer_spec.rb
+++ spec/models/offer_spec.rb
@@ -1,7 +1,25 @@
 require "rails_helper"
 
 RSpec.describe Offer, type: :model do
-  it { is_expected.to belong_to :subject }
-  it { is_expected.to belong_to :semester }
-  it { is_expected.to have_many :professors }
+  context "when testing associations..." do
+    it { is_expected.to belong_to :subject }
+    it { is_expected.to belong_to :semester }
+    it { is_expected.to have_many(:professors).through(:offer_professors) }
+    it { is_expected.to have_many(:offer_professors).dependent(:destroy) }
+  end
+
+  context "when testing validations..." do
+    it { is_expected.to validate_presence_of(:class_letter) }
+    it { is_expected.to validate_presence_of(:shift) }
+    it { is_expected.to define_enum_for(:shift).with(%i[day night]) }
+    it { is_expected.to validate_presence_of(:enrolled_count) }
+    it { is_expected.to validate_presence_of(:max_enrolled) }
+    it { is_expected.to validate_presence_of(:credits_of_theory) }
+    it { is_expected.to validate_presence_of(:credits_of_extension) }
+    it { is_expected.to validate_presence_of(:credits_of_practice) }
+    it { is_expected.to validate_presence_of(:credits_of_study) }
+    it { is_expected.to validate_presence_of(:obs1) }
+    it { is_expected.to validate_presence_of(:obs2) }
+    it { is_expected.to validate_presence_of(:obs3) }
+  end
 end
