b8a025d439acab3038cee9b74533dd570d01e5d9
alexnsouza6 <alexnsouza6@hotmail.com>
2018-11-10 12:40:06 -0200

    feat: sorting out contexts

--- app/models/flow_subject.rb
+++ app/models/flow_subject.rb
@@ -1,5 +1,8 @@
 class FlowSubject < ApplicationRecord
+  # Associations
   belongs_to :flow
   belongs_to :subject
+
+  # Validations
   validates :order, numericality: {only_integer: true}
 end
--- spec/models/flow_subject_spec.rb
+++ spec/models/flow_subject_spec.rb
@@ -1,7 +1,12 @@
 require "rails_helper"
 
 RSpec.describe FlowSubject, type: :model do
-  it { is_expected.to belong_to :flow }
-  it { is_expected.to belong_to :subject }
-  it { is_expected.to validate_numericality_of(:order).only_integer }
+  context "when testing associations..." do
+    it { is_expected.to belong_to :flow }
+    it { is_expected.to belong_to :subject }
+  end
+
+  context "when testing validations..." do
+    it { is_expected.to validate_numericality_of(:order).only_integer }
+  end
 end
