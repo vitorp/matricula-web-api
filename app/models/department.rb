81b17f7e207ddd00c12e3c18b2e7fb7a4b8eefc7
alexnsouza6 <alexnsouza6@hotmail.com>
2018-11-10 12:37:28 -0200

    feat: validations w/ tests and refactoring associations tests

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,5 +1,13 @@
 class Department < ApplicationRecord
+  # Associations
   has_many :courses, dependent: :destroy
   has_many :subjects, dependent: :destroy
   belongs_to :campus, optional: true
+
+  # Validations
+  with_options presence: true do
+    validates :name
+    validates :code
+    validates :initials
+  end
 end
--- spec/models/department_spec.rb
+++ spec/models/department_spec.rb
@@ -1,7 +1,15 @@
 require "rails_helper"
 
 RSpec.describe Department, type: :model do
-  it { is_expected.to have_many :courses }
-  it { is_expected.to have_many :subjects }
-  it { is_expected.to belong_to :campus }
+  context "when testing associations..." do
+    it { is_expected.to have_many(:courses).dependent(:destroy) }
+    it { is_expected.to have_many(:subjects).dependent(:destroy) }
+    it { is_expected.to belong_to(:campus) }
+  end
+
+  context "when testing validations..." do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:code) }
+    it { is_expected.to validate_presence_of(:initials) }
+  end
 end
