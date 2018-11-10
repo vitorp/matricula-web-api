5ad71739e9ae6bce4d29636273b9fd54d7f39278
alexnsouza6 <alexnsouza6@hotmail.com>
2018-11-10 12:51:47 -0200

    feat: sorting out contexts and adding name validation

--- spec/models/campus_spec.rb
+++ spec/models/campus_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Campus, type: :model do
   context "when testing associations" do
     it { is_expected.to have_many(:departments).dependent(:destroy) }
   end
+
   context "when testing validations" do
     it { is_expected.to validate_presence_of(:name) }
   end
