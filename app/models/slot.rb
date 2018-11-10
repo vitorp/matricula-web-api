ccf65c77e43a2e66978a03f886170edab2141073
alexnsouza6 <alexnsouza6@hotmail.com>
2018-11-10 12:49:13 -0200

    feat: validations w/ with_options and its tests

--- app/models/slot.rb
+++ app/models/slot.rb
@@ -1,3 +1,12 @@
 class Slot < ApplicationRecord
+  # Associations
   belongs_to :offer
+
+  # Validations
+  with_options presence: true do
+    validates :location
+    validates :weekday
+    validates :start_time
+    validates :end_time
+  end
 end
--- spec/models/slot_spec.rb
+++ spec/models/slot_spec.rb
@@ -1,5 +1,14 @@
 require "rails_helper"
 
 RSpec.describe Slot, type: :model do
-  it { is_expected.to belong_to :offer }
+  context "when testing associations..." do
+    it { is_expected.to belong_to :offer }
+  end
+
+  context "when testing validations..." do
+    it { is_expected.to validate_presence_of(:location) }
+    it { is_expected.to validate_presence_of(:weekday) }
+    it { is_expected.to validate_presence_of(:start_time) }
+    it { is_expected.to validate_presence_of(:end_time) }
+  end
 end
