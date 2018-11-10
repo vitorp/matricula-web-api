905702eb80d66f48d8861db7f86ed5f7ad812ef2
alexnsouza6 <alexnsouza6@hotmail.com>
2018-11-10 12:47:01 -0200

    feat: Adding validations and its tests

--- app/models/semester.rb
+++ app/models/semester.rb
@@ -1,4 +1,11 @@
 class Semester < ApplicationRecord
+  # Associations
   has_many :offers, dependent: :destroy
   has_many :subjects, through: :offers
+
+  # Validations
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :period, presence: true
+  validates :year, presence: true
 end
--- spec/models/semester_spec.rb
+++ spec/models/semester_spec.rb
@@ -1,8 +1,17 @@
 require "rails_helper"
 
 RSpec.describe Semester, type: :model do
-  it { is_expected.to have_many :offers }
-  it { is_expected.to have_many :subjects }
+  context "when testing associations..." do
+    it { is_expected.to have_many(:offers).dependent(:destroy) }
+    it { is_expected.to have_many(:subjects).through(:offers) }
+  end
+
+  context "when testing validations..." do
+    it { is_expected.to validate_presence_of(:start_date) }
+    it { is_expected.to validate_presence_of(:end_date) }
+    it { is_expected.to validate_presence_of(:period) }
+    it { is_expected.to validate_presence_of(:year) }
+  end
 
   describe ".subjects" do
     it "returns offer subject" do
