c1279b2b4ef86083af6b2dd05e550a62e0f29d7a
alexnsouza6 <alexnsouza6@hotmail.com>
2018-11-10 12:43:56 -0200

    feat: validations w/ tests and refactoring associations tests

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -1,4 +1,8 @@
 class Professor < ApplicationRecord
+  # Associations
   has_many :offer_professors, dependent: :destroy
   has_many :offers, through: :offer_professors
+
+  # Validations
+  validates :name, presence: true
 end
--- spec/models/professor_spec.rb
+++ spec/models/professor_spec.rb
@@ -1,5 +1,12 @@
 require "rails_helper"
 
 RSpec.describe Professor, type: :model do
-  it { is_expected.to have_many :offers }
+  context "when testing associations..." do
+    it { is_expected.to have_many(:offers).through(:offer_professors) }
+    it { is_expected.to have_many(:offer_professors).dependent(:destroy) }
+  end
+
+  context "when testing validations..." do
+    it { is_expected.to validate_presence_of(:name) }
+  end
 end
